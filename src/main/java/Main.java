9444356598d31cdb60c9179eca9665d66094582e
zhangyang <zhangyang03@imdada.cn>
2019-08-29 14:40:06 +0800

    hhahaha

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author 张洋
+ * @date: 2019-08-07 14:15
+ */
+public class Main {
+}
---
+++ src/test/java/FloatTest.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author 张洋
+ * @date: 2019-08-14 16:39
+ */
+public class FloatTest {
+}
